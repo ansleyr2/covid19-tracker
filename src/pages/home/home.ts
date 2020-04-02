3073474bc8e626b299e9cd85a40894f5755a7584
Ansley Rodrigues <ansleyr2@gmail.com>
2020-04-02 13:57:29 -0400

    Added more details for selected country

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,9 +10,19 @@ import { ModalPage } from '../modal/modal';
 interface todayData{
   todayCases? : number;
   todayDeaths ? : number;
+  active?: number;
+  critical? : number;
   casesPerOneMillion? : number;
   deathsPerOneMillion? : number;
+  updated? : string;
   isError? : boolean
+}
+
+interface globalData{
+  confirmed?: number;
+  recovered?: number;
+  deaths?: number;
+  updateTime?: any;
 }
 
 @Component({
@@ -21,7 +31,7 @@ interface todayData{
 })
 export class HomePage {
 
-  globalData: any = null;
+  globalData: globalData = {};
   deathPercentage: any = null;
   recoveryPercentage: any = null;
 
@@ -49,28 +59,47 @@ export class HomePage {
     this.countries = [];
     this.resetSearchResults();
     this.appSvc.getCountriesList().subscribe((data: any) => {
-      // console.log(data.countries);
-      const countriesObj = data.countries;
+      //console.log(data.countries);
+      // const countriesObj = data.countries;
 
-      const entries = Object.entries(countriesObj);
-      //console.log(entries);
+      //const entries = Object.entries(countriesObj);
+      // console.log(entries);
 
       // for (const [countryFullName, countryISOCode] of entries) {
       //   this.countries.push({ isoCode: countryISOCode, name: countryFullName });
       // }
 
       // changes as per api modifications
-      for (const [id, countryDetails] of entries) {
+      // for (const [id, countryDetails] of data) {
+      //   console.log(id);
+      //   const details: any = countryDetails;
+      //   this.countries.push({ isoCode: details.iso2, name: details.name });
+      // }
+
+      /*for (const [country, countryInfo] of data) {
+        //console.log(country, countryInfo);
         const details: any = countryDetails;
         this.countries.push({ isoCode: details.iso2, name: details.name });
+      }*/
+      if(data.length > 0){
+        data.forEach((country: any)=>{
+          const name = country.country;
+          const isoCode = country.countryInfo.iso2;
+          this.countries.push({ isoCode: isoCode, name: name });
+        })
+  
+        this.countries.sort(function (a, b) {
+          return ('' + a.name).localeCompare(b.name);
+        })
       }
-
+     
       console.log(this.countries);
 
 
       this.selectedCountry? this.getCasesByCountry(this.selectedCountry)  : this.getGlobalCounts();
 
     }, err => {
+      this.loader.dismiss();
       console.log("Error while fetching countries");
     })
 
@@ -79,22 +108,36 @@ export class HomePage {
   getGlobalCounts(refresher?: any, isRefresh = false) {
     this.showLoader();
     this.appSvc.getGlobalCounts().subscribe((data: any) => {
+    // this.appSvc.getGlobalCounts_1().subscribe((data: any) => {
       if(isRefresh){
         refresher.complete();
       }
       this.loader.dismiss();
       // console.log(data);
-      this.globalData = data;
+      // this.globalData = data;
+
+      /*this.globalData.confirmed = data.cases;
+      this.globalData.recovered = data.recovered;
+      this.globalData.deaths = data.deaths;*/
+
+      this.globalData.confirmed = data.confirmed.value;
+      this.globalData.recovered = data.recovered.value;
+      this.globalData.deaths = data.deaths.value;
 
       const confirmed = data.confirmed.value;
       const deaths = data.deaths.value;
       const recoveries = data.recovered.value;
+
+      /*const confirmed = data.cases;
+      const deaths = data.deaths;
+      const recoveries = data.recovered;*/
 
       this.deathPercentage = ((deaths / confirmed) * 100).toFixed(2);
       this.recoveryPercentage = ((recoveries / confirmed) * 100).toFixed(2);
 
       this.lastUpdateTime = new Date(data.lastUpdate).toLocaleDateString() + " " + new Date(data.lastUpdate).toLocaleTimeString();
     }, err => {
+      this.loader.dismiss();
       console.log("Error");
     })
   }
@@ -105,16 +148,6 @@ export class HomePage {
     this.resetSearchResults();
     this.selectedCountry = null;
   }
-
-  // clear(event?) {
-  //   console.log("clear");
-  //   this.isSearch = false;
-  //   this.resetSearchResults();
-  //   // this.query = "";
-  //   //this.getGlobalCounts();
-  //   this.selectedCountry = null;
-  //   this.todayData = {isError: true};
-  // }
 
   onCancel(event?) {
     console.log("onCancel");
@@ -130,6 +163,10 @@ export class HomePage {
     console.log(this.query);
 
     if (this.query.length === 0) {
+      this.isSearch = false;
+      this.todayData.isError = true;
+      this.selectedCountry = null;
+
       this.getGlobalCounts();
       return;
     }
@@ -155,13 +192,14 @@ export class HomePage {
 
   getCasesByCountry(country: any, refresher?: any, isRefresh = false){
     this.appSvc.getCountByCountry(country).subscribe((data: any) => {
-      this.getTodayData(country);
       if(isRefresh){
         refresher.complete();
       }
       this.loader.dismiss();
-      // console.log(data);
-      this.globalData = data;
+
+      this.globalData.confirmed = data.confirmed.value;
+      this.globalData.recovered = data.recovered.value;
+      this.globalData.deaths = data.deaths.value;
 
       const confirmed = data.confirmed.value;
       const deaths = data.deaths.value;
@@ -171,7 +209,19 @@ export class HomePage {
       this.recoveryPercentage = ((recoveries / confirmed) * 100).toFixed(2);
       this.lastUpdateTime = new Date(data.lastUpdate).toLocaleDateString() + " " + new Date(data.lastUpdate).toLocaleTimeString();
 
+      // console.log(data);
+     // this.globalData = data;
+
+      /*const confirmed = data.confirmed.value;
+      const deaths = data.deaths.value;
+      const recoveries = data.recovered.value;
+
+      this.deathPercentage = ((deaths / confirmed) * 100).toFixed(2);
+      this.recoveryPercentage = ((recoveries / confirmed) * 100).toFixed(2);
+      this.lastUpdateTime = new Date(data.lastUpdate).toLocaleDateString() + " " + new Date(data.lastUpdate).toLocaleTimeString();*/
+
     }, err => {
+      this.loader.dismiss();
       console.log("Error while fetching country data");
     })
   }
@@ -188,6 +238,7 @@ export class HomePage {
       this.presentModal(data, 'C');
     }, err => {
       console.log("error...");
+      this.loader.dismiss();
     })
   }
 
@@ -198,6 +249,7 @@ export class HomePage {
       this.presentModal(data, 'R');
     }, err => {
       console.log("error...");
+      this.loader.dismiss();
     })
   }
 
@@ -208,14 +260,16 @@ export class HomePage {
       this.presentModal(data, 'D');
     }, err => {
       console.log("error...");
+      this.loader.dismiss();
     })
   }
 
-  async presentModal(data: any, type: string) {
+  async presentModal(data: any, type: string, moreInfo: boolean = false) {
     const modal = this.modalController.create(ModalPage, {
       data: data,
       type: type,
-      countrySearched: this.selectedCountry ? true : false
+      countrySearched: this.selectedCountry ? true : false,
+      moreInfo
     });
     modal.onDidDismiss(data => {
       console.log("modal.onDidDismiss");
@@ -242,23 +296,46 @@ export class HomePage {
     }
   }
 
-  getTodayData(country){
-    this.appSvc.getCasesAndTodaysCountsByCountry(country).subscribe((res: any)=>{
+  getTodayData(){
+    this.showLoader();
+    this.appSvc.getCasesAndTodaysCountsByCountry(this.selectedCountry).subscribe((res: any)=>{
+      /*this.globalData.confirmed = res.cases;
+      this.globalData.recovered = res.recovered;
+      this.globalData.deaths = res.deaths;
+
+
+      const confirmed = res.cases;
+      const deaths = res.deaths;
+      const recoveries = res.recovered;
+
+      this.deathPercentage = ((deaths / confirmed) * 100).toFixed(2);
+      this.recoveryPercentage = ((recoveries / confirmed) * 100).toFixed(2);*/
+     // this.lastUpdateTime = new Date(data.lastUpdate).toLocaleDateString() + " " + new Date(data.lastUpdate).toLocaleTimeString();
+
       const todayCases = res.todayCases;
       const todayDeaths = res.todayDeaths;
       const casesPerOneMillion = res.casesPerOneMillion;
       const deathsPerOneMillion = res.deathsPerOneMillion;
+      const active = res.active;
+      const critical = res.critical;
+      const updated = res.updated;
       this.todayData ={
         todayCases,
         todayDeaths,
         casesPerOneMillion,
         deathsPerOneMillion,
+        active,
+        critical,
+        updated : new Date(updated).toLocaleDateString() + ' ' + new Date(updated).toLocaleTimeString(),
         isError: false
       }
       console.log(todayCases, todayDeaths, casesPerOneMillion, deathsPerOneMillion);
+
+      this.presentModal(this.todayData, '', true);
     }, err =>{
       console.log(err);
       this.todayData.isError = true;
+      this.loader.dismiss();
     });
   }
 }
--- src/pages/modal/modal.ts
+++ src/pages/modal/modal.ts
@@ -11,11 +11,13 @@ export class ModalPage {
     data: any[] = [];
     type: string;
     countrySearched: boolean = false;
+    showMoreInfo: boolean = false;
     constructor(public viewController: ViewController, public params: NavParams) {
         console.log('params', params.data.data, params.data.type);
         this.data = params.data.data;
         this.type = params.data.type;
         this.countrySearched = params.data.countrySearched;
+        this.showMoreInfo = params.data.moreInfo;
     }
 
     dismiss() {
