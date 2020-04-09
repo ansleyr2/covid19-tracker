b5a2348bc8db645bb0aebc919f48a389f2d3c657
Ansley Rodrigues <ansleyr2@gmail.com>
2020-04-09 17:11:14 -0400

    Added documentation

--- build-release.sh
+++ build-release.sh
@@ -4,19 +4,21 @@ echo ""
 echo "DID YOU UPDATE THE APP VERSION?"
 echo "TYPE 1 OR 2"
 
-# Operating system names are used here as a data source
+# Checking if the user has updated the app version before continuing signing the app.
 select answer in YES NO
 do
 
 case $answer in
 # YES SELECTED
 "YES")
+echo "YOU SELECTED $answer."
 rm platforms/android/app/build/outputs/apk/release/*
 echo ""
 # Prompt for password
 read -s -p "Enter Keystore Password: " password
 cordova build android --release -- --keystore=./covid19-release-key.keystore --storePassword=$password --alias=covid19-release-key --password=$password
-echo "YOU SELECTED $answer."
+# After build is success, the scripts/afterBuild.js is invoked by ionic after_Build hook mentioned in config.xml
+# The genertaed signed build is uploaded to google drive.
 break
 ;;
 # NO SELECTED
